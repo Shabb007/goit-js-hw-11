e9473160ab11448b76c32499091122c58b75cdc4
Sebnem <sebkay2002@hotmail.com>
2024-11-23 23:38:38 +0300

    f

---
+++ main.js
@@ -0,0 +1,56 @@
+import { fetchData } from './src/js/pixabay-api.js';
+import { renderImages, showNoResultsMessage } from './src/js/render-functions.js';
+
+const form = document.querySelector(".submit-form");
+const list = document.querySelector(".list");
+const loader = document.getElementById("loader");
+
+if (typeof global === 'undefined') {
+    var global = window;
+}
+
+
+function showLoader() {
+    loader.style.display = "block";
+}
+
+function hideLoader() {
+    loader.style.display = "none";
+}
+
+function handleSearch(event) {
+    event.preventDefault();
+
+    const query = event.currentTarget.elements.searchQuery.value.trim();
+    if (query === "") {
+        iziToast.warning({
+            title: "Warning",
+            message: "Please enter a search query!",
+            position: "topRight"
+        });
+        return;
+    }
+
+    showLoader(); 
+
+    fetchData(query)
+        .then((data) => {
+            hideLoader(); 
+            if (data.hits.length === 0) {
+                showNoResultsMessage();
+            } else {
+                renderImages(data.hits, list);
+            }
+        })
+        .catch(error => {
+            hideLoader(); 
+            console.error("Error:", error);
+            iziToast.error({
+                title: "Error",
+                message: "Something went wrong. Please try again later.",
+                position: "topRight"
+            });
+        });
+}
+
+form.addEventListener("submit", handleSearch);
